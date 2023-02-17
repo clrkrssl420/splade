daa134640fb98aa13da721f502dc29ae811de67f
Clark Russel <luyao.clarkrussel@gmail.com>
2023-02-17 09:37:17 +0800

    added argon dashboard assets and links

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -9,6 +9,13 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        {{-- Font Awesome --}}
+        <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+        {{-- Icons --}}
+        <link href="{{ URL::asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+        <link href="{{ URL::asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+        {{-- Main Argon CSS --}}
+        <link href="{{ URL::asset('assets/css/argon-dashboard-tailwind.css') }}" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/js/app.js'])
@@ -16,5 +23,9 @@
     </head>
     <body class="font-sans antialiased">
         @splade
+        {{-- Perfect Scrollbar --}}
+        <script src="{{ URL::asset('assets/js/perfect-scrollbar.js') }}" async></script>
+        <!-- main script file  -->
+        <script src="{{ URL::asset('assets/js/argon-dashboard-tailwind.js') }}" async></script>
     </body>
 </html>
