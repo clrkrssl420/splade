a81cb51d99ce747f7e2374b826ae2ef54a45565f
Clark Russel <luyao.clarkrussel@gmail.com>
2023-02-17 16:23:40 +0800

    leads and lead status model and controller

---
+++ app/Http/Controllers/LeadsController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lead;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\LeadStatus;
+use ProtoneMedia\Splade\SpladeTable;
+use App\Http\Requests\StoreLeadRequest;
+use App\Http\Requests\UpdateLeadRequest;
+use Symfony\Component\HttpFoundation\Response;
+
+class LeadsController extends Controller
+{
+    public function index()
+    {
+
+        $leads = Lead::with(['lead_status'])->paginate();
+        // $leads = Lead::all();
+
+        return view('leads.index', [
+            'leads' => SpladeTable::for($leads)
+                ->column('id')
+                ->column('phone')
+                ->column('description')
+                ->column('lead_status_id', 'Status'),
+        ]);
+    }
+
+    public function create()
+    {
+
+        $users = User::pluck('name', 'id')->prepend(trans('global.pleaseSelect'), '');
+
+        // $lead_statuses = LeadStatus::pluck('status', 'id')->prepend(trans('global.pleaseSelect'), '');
+
+        return view('admin.leads.create', compact('lead_statuses', 'users'));
+    }
+
+    public function store(StoreLeadRequest $request)
+    {
+        $lead = Lead::create($request->all());
+
+        return redirect()->route('admin.leads.index');
+    }
+
+    public function edit(Lead $lead)
+    {
+
+        $users = User::pluck('name', 'id')->prepend(trans('global.pleaseSelect'), '');
+
+        // $lead_statuses = LeadStatus::pluck('status', 'id')->prepend(trans('global.pleaseSelect'), '');
+
+        $lead->load('user', 'lead_status');
+
+        return view('admin.leads.edit', compact('lead', 'lead_statuses', 'users'));
+    }
+
+    public function update(UpdateLeadRequest $request, Lead $lead)
+    {
+        $lead->update($request->all());
+
+        return redirect()->route('admin.leads.index');
+    }
+
+    public function destroy(Lead $lead)
+    {
+
+        $lead->delete();
+
+        return back();
+    }
+}
---
+++ app/Models/Lead.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use \DateTimeInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Lead extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+
+    public $table = 'leads';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'phone',
+        'description',
+        'user_id',
+        'lead_status_id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function lead_status()
+    {
+        return $this->belongsTo(LeadStatus::class, 'lead_status_id');
+    }
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
