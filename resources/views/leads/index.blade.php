e8e98b77d00fba8f39af0c6d398f90be0f6a62f9
Clark Russel <luyao.clarkrussel@gmail.com>
2023-02-17 16:25:21 +0800

    added leads index page

---
+++ resources/views/leads/index.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        My Leads
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <x-splade-table :for="$leads" as="$lead">
+                        <x-splade-cell lead_status_id>
+                            {{ $lead->lead_status->status ?? ''}}
+                        </x-splade-cell>
+                    </x-splade-table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
