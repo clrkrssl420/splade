8f5e66afa568d51d502c47f8f401dee1cb98ee7b
Clark Russel <luyao.clarkrussel@gmail.com>
2023-02-17 16:25:08 +0800

    added chirp and leads link to sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -20,7 +20,16 @@
         </li>
 
         <li class="mt-0.5 w-full">
-            <Link href="{{ route('dashboard') }}" class="py-2.7 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors {{ request()->is("leads") || request()->is("leads/*") ? "bg-blue-500/13" : "" }}">
+            <Link href="{{ route('chirps.index') }}" class="py-2.7 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors {{ request()->is("chirps") || request()->is("chirps/*") ? "bg-blue-500/13" : "" }}">
+            <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                <i class="relative top-0 text-sm leading-normal text-blue-500 ni ni-satisfied"></i>
+            </div>
+            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Chirps</span>
+            </Link>
+        </li>
+
+        <li class="mt-0.5 w-full">
+            <Link href="{{ route('leads.index') }}" class="py-2.7 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors {{ request()->is("leads") || request()->is("leads/*") ? "bg-blue-500/13" : "" }}">
             <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                 <i class="relative top-0 text-sm leading-normal text-blue-500 ni ni-satisfied"></i>
             </div>
